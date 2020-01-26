06163b57b9aa146cd9b1624120a0648c82018227
Emily Kuo <em.s.kuo@gmail.com>
2020-01-25 22:50:52 -0800

    created Customer class and function to read customer data from file in customers.py

--- customers.py
+++ customers.py
@@ -5,3 +5,29 @@ class Customer(object):
     """Ubermelon customer."""
 
     # TODO: need to implement this
+    def __init__(self, first_name, last_name, email, password):
+        """Instantiate a Customer"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        """Representation of a Customer"""
+
+        return f"<Customer: {self.first_name}, {self.last_name}"
+
+
+def read_customer_data(filepath):
+    """Read customer data from file and return dictionary"""
+
+    customers = {}
+
+    with open(filepath) as file:
+        for line in file:
+            (first_name, last_name, email, password) = line.strip().split("|")
+
+            customers[email] = Customer(first_name, last_name, email, password)
+
+    return customers
